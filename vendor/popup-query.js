694a81ee92b9d6aac48c832bdc2248b53f53782e
charlest7 <charlestendeanz@gmail.com>
2017-05-07 18:26:16 +0700

    Create popup-query.js

---
+++ vendor/popup-query.js
@@ -0,0 +1,11 @@
+<script>
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(-6.226238, 106.808438),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("map"),mapProp);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5DYaK8NkLUXUmROK2ujhQHlmEzOKaH4E&callback=myMap"></script>
