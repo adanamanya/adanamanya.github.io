569b55c33d7e9c37c799c26f24d5c37f962b0107
charlest7 <charlestendeanz@gmail.com>
2017-06-07 18:51:56 +0700

    Update popup-query.js

--- vendor/popup-query.js
+++ vendor/popup-query.js
@@ -1,11 +1,16 @@
 <script>
 function myMap() {
 var mapProp= {
-    center:new google.maps.LatLng(-6.226238, 106.808438),
+    center:new google.maps.LatLng(51.508742,-0.120850),
     zoom:5,
 };
-var map=new google.maps.Map(document.getElementById("map"),mapProp);
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 </script>
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5DYaK8NkLUXUmROK2ujhQHlmEzOKaH4E&callback=myMap"></script>
+
+
+
+
+
