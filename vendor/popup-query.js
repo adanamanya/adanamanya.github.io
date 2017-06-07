3dfb0471c6bf1f57169674d6bcb70a7627ed4665
charlest7 <charlestendeanz@gmail.com>
2017-06-07 16:16:00 +0700

    Update popup-query.js

--- vendor/popup-query.js
+++ vendor/popup-query.js
@@ -1,5 +1,4 @@
 <script>
-
 function myMap() {
 var mapProp= {
     center:new google.maps.LatLng(-6.226238, 106.808438),
@@ -10,7 +9,3 @@ var map=new google.maps.Map(document.getElementById("map"),mapProp);
 </script>
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5DYaK8NkLUXUmROK2ujhQHlmEzOKaH4E&callback=myMap"></script>
-
-
-
-
